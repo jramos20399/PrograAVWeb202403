eec6f704bcce6c8e2322e89ef287b5d55be4b1c4
Pablo Ramos <juanpabloramosp@hotmail.com>
2024-09-24 18:37:07 -0600

    Mapeo Terminado

---
+++ Northwind202403/Entities/Entities/CurrentProductList.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Entities.Entities;
+
+public partial class CurrentProductList
+{
+    public int ProductId { get; set; }
+
+    public string ProductName { get; set; } = null!;
+}
---
+++ Northwind202403/Entities/Entities/ProductsAboveAveragePrice.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Entities.Entities;
+
+public partial class ProductsAboveAveragePrice
+{
+    public string ProductName { get; set; } = null!;
+
+    public decimal? UnitPrice { get; set; }
+}
