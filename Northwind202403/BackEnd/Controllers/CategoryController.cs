79717ea9414018bfc07796ddc8c5539bf1e73028
Pablo Ramos <juanpabloramosp@hotmail.com>
2024-09-24 19:25:16 -0600

    Dependency Injection

---
+++ Northwind202403/BackEnd/Controllers/CategoryController.cs
@@ -0,0 +1,54 @@
+﻿using BackEnd.Services.Interfaces;
+using Entities.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BackEnd.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        ICategoryService categoryService;
+
+        public CategoryController(ICategoryService categoryService)
+        {
+            this.categoryService = categoryService;
+        }
+
+        // GET: api/<CategoryController>
+        [HttpGet]
+        public IEnumerable<Category> Get()
+        {
+
+
+            return categoryService.Obtener();
+        }
+
+        // GET api/<CategoryController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<CategoryController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<CategoryController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<CategoryController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
--- Northwind202403/BackEnd/Program.cs
+++ Northwind202403/BackEnd/Program.cs
@@ -1,3 +1,10 @@
+using BackEnd.Services.Implementations;
+using BackEnd.Services.Interfaces;
+using DAL.Implementations;
+using DAL.Interfaces;
+using Entities.Entities;
+using Microsoft.Data.SqlClient;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +13,16 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+#region DI
+builder.Services.AddDbContext<NorthWindContext>();
+builder.Services.AddScoped<IUnidadDeTrabajo,UnidadDeTrabajo>();
+builder.Services.AddScoped<ICategoryDAL, CategoryDALImpl>();
+builder.Services.AddScoped<ICategoryService, CategoryService>();
+
+
+#endregion
+
 
 var app = builder.Build();
 
