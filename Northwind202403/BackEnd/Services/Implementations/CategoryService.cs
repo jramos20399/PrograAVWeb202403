624e4a34be0bfb5e93c28fdd19c2a990d1533c92
Pablo Ramos <juanpabloramosp@hotmail.com>
2024-09-24 19:10:38 -0600

    Services completed

---
+++ Northwind202403/BackEnd/Services/Implementations/CategoryService.cs
@@ -0,0 +1,45 @@
+﻿using BackEnd.Services.Interfaces;
+using DAL.Interfaces;
+using Entities.Entities;
+
+namespace BackEnd.Services.Implementations
+{
+    public class CategoryService : ICategoryService
+    {
+        IUnidadDeTrabajo Unidad;
+
+        public CategoryService(IUnidadDeTrabajo unidadDeTrabajo)
+        {
+            this.Unidad = unidadDeTrabajo;
+        }
+
+
+        public bool Agregar(Category category)
+        {
+           Unidad.CategoryDAL.Add(category);
+            return Unidad.Complete();
+        }
+
+        public bool Editar(Category category)
+        {
+            Unidad.CategoryDAL.Update(category);
+            return Unidad.Complete();   
+        }
+
+        public bool Eliminar(Category category)
+        {
+            Unidad.CategoryDAL.Remove(category);
+            return Unidad.Complete();
+        }
+
+        public Category Obtener(int id)
+        {
+            return Unidad.CategoryDAL.Get(id);
+        }
+
+        public List<Category> Obtener()
+        {
+            return Unidad.CategoryDAL.GetAll().ToList(); 
+        }
+    }
+}
---
+++ Northwind202403/BackEnd/Services/Interfaces/ICategoryService.cs
@@ -0,0 +1,24 @@
+﻿using Entities.Entities;
+
+namespace BackEnd.Services.Interfaces
+{
+    public interface ICategoryService
+    {
+
+        bool Agregar(Category category);
+        bool Editar(Category category);
+        bool Eliminar(Category category);
+        /// <summary>
+        /// Obtiene Category por Id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        Category Obtener(int id);
+        
+        /// <summary>
+        /// Obtiene todas la categories
+        /// </summary>
+        /// <returns></returns>
+        List<Category> Obtener();
+    }
+}
