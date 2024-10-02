75a76cdccb462e56839ef3fce657e34b3a5b613e
Pablo Ramos <juanpabloramosp@hotmail.com>
2024-10-01 18:47:05 -0600

    CRUD category BE

--- Northwind202403/BackEnd/Controllers/CategoryController.cs
+++ Northwind202403/BackEnd/Controllers/CategoryController.cs
@@ -1,6 +1,6 @@
 ﻿using BackEnd.DTO;
 using BackEnd.Services.Interfaces;
-using Entities.Entities;
+
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -36,20 +36,31 @@ namespace BackEnd.Controllers
 
         // POST api/<CategoryController>
         [HttpPost]
-        public void Post([FromBody] string value)
+        public IActionResult Post([FromBody] CategoryDTO category )
         {
+            categoryService.Agregar(category);
+
+            return Ok(category);
         }
 
         // PUT api/<CategoryController>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        [HttpPut]
+        public IActionResult Put([FromBody] CategoryDTO  category)
         {
+            categoryService.Editar(category);
+            return Ok(category);
+
         }
 
         // DELETE api/<CategoryController>/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
+            CategoryDTO category = new CategoryDTO
+            {
+                CategoryId = id
+            };
+            categoryService.Eliminar(category); 
         }
     }
 }
