d5d62539b01db1cf91697195dad78abdc8652804
Pablo Ramos <juanpabloramosp@hotmail.com>
2024-10-08 19:16:10 -0600

    Etiquetas.

--- Northwind202403/FrontEnd/Models/CategoryViewModel.cs
+++ Northwind202403/FrontEnd/Models/CategoryViewModel.cs
@@ -1,12 +1,17 @@
-﻿namespace FrontEnd.Models
+﻿using Microsoft.Build.Framework;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace FrontEnd.Models
 {
     public class CategoryViewModel
     {
 
 
-
+        [DisplayName("ID")]
         public int CategoryId { get; set; }
-
+        [DisplayName("Name")]
+        [System.ComponentModel.DataAnnotations.Required(ErrorMessage = "Nombre es requerido")]
         public string CategoryName { get; set; } = null!;
     }
 }
