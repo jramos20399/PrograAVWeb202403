6a81c22db17c8b7a36958ecdb2e808eedf253fbc
Pablo Ramos <juanpabloramosp@hotmail.com>
2024-10-15 18:34:58 -0600

    Mapeo DTO

---
+++ Northwind202403/FrontEnd/ApiModels/Product.cs
@@ -0,0 +1,16 @@
+﻿namespace FrontEnd.ApiModels
+{
+    public class Product
+    {
+
+        public int ProductId { get; set; }
+       
+        public string ProductName { get; set; } = null!;
+       
+        public int? SupplierId { get; set; }
+        
+        public int? CategoryId { get; set; }
+
+        public bool Discontinued { get; set; }
+    }
+}
---
+++ Northwind202403/FrontEnd/ApiModels/Supplier.cs
@@ -0,0 +1,11 @@
+﻿namespace FrontEnd.ApiModels
+{
+    public class Supplier
+    {
+
+
+        public int SupplierId { get; set; }
+
+        public string CompanyName { get; set; } = null!;
+    }
+}
