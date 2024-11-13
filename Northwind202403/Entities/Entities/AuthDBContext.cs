026f7fdce1c3d0c53b82de548b53c63c1c088b40
Pablo Ramos <juanpabloramosp@hotmail.com>
2024-11-12 18:36:48 -0600

    AuthDbContext

---
+++ Northwind202403/Entities/Entities/AuthDBContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Entities
+{
+    public class AuthDBContext : IdentityDbContext
+    {
+
+        public AuthDBContext(DbContextOptions<AuthDBContext> options)
+      : base(options)
+        {
+        }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        { 
+            base.OnModelCreating(modelBuilder);
+        
+        }
+        }
+}
